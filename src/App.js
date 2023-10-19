9fcbadc616a5b7ffe3af6c5090e21934757e41dd
unbearabletruth <antaarty@gmail.com>
2023-10-19 15:14:10 +0700

    single post page, edit post

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import {
 import { Navbar } from './app/Navbar'
 import { PostsList } from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
+import { SinglePostPage } from './features/posts/SinglePostPage'
+import { EditPostForm } from './features/posts/EditPostForm'
 
 function App() {
   return (
@@ -25,6 +27,8 @@ function App() {
               </React.Fragment>
             )}
           />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
           <Redirect to="/" />
         </Switch>
       </div>
