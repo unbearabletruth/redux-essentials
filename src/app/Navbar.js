1fe2870e36e908324ce3fa50338ae077aa2e2b7a
unbearabletruth <antaarty@gmail.com>
2023-10-23 17:07:06 +0700

    Add new notifications, show unread number

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { fetchNotifications } from '../features/notifications/notificationsSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  fetchNotifications,
+  selectAllNotifications
+} from '../features/notifications/notificationsSlice'
 
 export const Navbar = () => {
   const dispatch = useDispatch()
+  const notifications = useSelector(selectAllNotifications)
+  const numUnreadNotifications = notifications.filter(n => !n.read).length
 
   const fetchNewNotifications = () => {
     dispatch(fetchNotifications())
+  }
+
+  let unreadNotificationsBadge
+
+  if (numUnreadNotifications > 0) {
+    unreadNotificationsBadge = (
+      <span className="badge">{numUnreadNotifications}</span>
+    )
   }
 
   return (
@@ -19,7 +32,9 @@ export const Navbar = () => {
           <div className="navLinks">
             <Link to="/">Posts</Link>
             <Link to="/users">Users</Link>
-            <Link to="/notifications">Notifications</Link>
+            <Link to="/notifications">
+              Notifications {unreadNotificationsBadge}
+            </Link>
           </div>
           <button className="button" onClick={fetchNewNotifications}>
             Refresh Notifications
--- src/features/notifications/notificationsSlice.js
+++ src/features/notifications/notificationsSlice.js
@@ -7,7 +7,7 @@ export const fetchNotifications = createAsyncThunk(
   async (_, { getState }) => {
     const allNotifications = selectAllNotifications(getState())
     const [latestNotification] = allNotifications
-    const latestTimestamp = latestNotification ? latestNotification.date : ''
+        const latestTimestamp = latestNotification ? latestNotification.date : ''
     const response = await client.get(
       `/fakeApi/notifications?since=${latestTimestamp}`
     )
@@ -18,15 +18,26 @@ export const fetchNotifications = createAsyncThunk(
 const notificationsSlice = createSlice({
   name: 'notifications',
   initialState: [],
-  reducers: {},
+  reducers: {
+    allNotificationsRead(state, action) {
+      state.forEach(notification => {
+        notification.read = true
+      })
+    }
+  },
   extraReducers(builder) {
     builder.addCase(fetchNotifications.fulfilled, (state, action) => {
       state.push(...action.payload)
-      // Sort with newest first
+      state.forEach(notification => {
+        // Any notifications we've read are no longer new
+        notification.isNew = !notification.read
+      })
       state.sort((a, b) => b.date.localeCompare(a.date))
     })
   }
 })
+
+export const { allNotificationsRead } = notificationsSlice.actions
 
 export default notificationsSlice.reducer
 
